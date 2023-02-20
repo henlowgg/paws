7b6dd25ee2d9185f78eb5f2189747c2ee0d3d838
Jack Ault <henlowgg@outlook.com>
2023-02-20 17:41:05 -0500

    successfully connected to the eth network with hardhat and got back a paws addressgit add .

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -5,4 +5,13 @@ const { ethers } = require('hardhat')
 const main = async () => {
   const pawsFactory = await ethers.getContractFactory('pawsERC721')
   const pawsContract = await pawsFactory.deploy()
+
+  console.log('PAWS CONTRACT ADDRESS:', pawsContract.address)
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.log('Error in deploying Contract >> ', error)
+    process.exit(1)
+  })
