fa27c1b6f846366e5c19014ed2197756b969186d
Jack Ault <henlowgg@outlook.com>
2023-02-20 17:03:01 -0500

    adding a deployable contract

--- smart-contract/contracts/pawsERC721.sol
+++ smart-contract/contracts/pawsERC721.sol
@@ -8,12 +8,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract pawsERC721 is ERC721URIStorage {
 
 // declaring sol variable
-    uint256 PAWS_TOKEN_id;
+    uint256 PAWS_TOKEN_ID;
 
         constructor() ERC721("CleverNFT", "CN") {}
-
+        
+        // this function is going to generate an NFT based on if two people match with each other, and it will send this minted NFT to both of their wallet addresses
         function mintNFT(address _userOne, address _userTwo, string memory tokenURI) public {
+            _mint(_userOne, PAWS_TOKEN_ID);
+            _setTokenURI(PAWS_TOKEN_ID, tokenURI);
+                PAWS_TOKEN_ID++;
             
+            _mint(_userTwo, PAWS_TOKEN_ID);
+            _setTokenURI(PAWS_TOKEN_ID, tokenURI);
+                PAWS_TOKEN_ID++;
         }
 
-};
+}
